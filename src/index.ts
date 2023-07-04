92631c4e0794f000646e00283c86d60a0541ab06
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-05 00:05:48 +0300

    added src/index.ts file

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Application, Response, Request } from 'express'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+
+
+
+dotenv.config()
+
+
+const PORT = process.env.PORT || 3001
+
+const app: Application = express()
+const allowedOrigins = ['http://localhost:3000/'];
+
+const options: cors.CorsOptions = {
+  origin: "*"
+};
+app.use(cors(options));
+
+app.use(express.json())
+
+
+
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World YAaaaaaaaaa' })
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
+
+export default app 
