2119282e85645343403714e0267719c2d8e86cf7
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-05 01:03:30 +0300

    update database/index.ts

---
+++ src/database/index.ts
@@ -0,0 +1,33 @@
+import { Pool } from 'pg'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_MAX
+} = process.env
+
+let db: Pool
+
+
+  db = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    port: parseInt(POSTGRES_PORT as string),
+    max: parseInt(POSTGRES_MAX as string)
+  })
+
+
+
+db.on('error', (error: Error) => {
+  console.error(error.message)
+})
+
+export default db
