18ff889257bc2d603f8b4f1c8841ad84c9c0cca3
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-05 01:16:58 +0300

    adding types directory for user and tracking types

---
+++ src/types/tracking.type.ts
@@ -0,0 +1,17 @@
+type User = {
+    id: number,
+    sender_name: string,
+    sender_phone: string,
+    sender_adress: string,
+    receiver_name: string,
+    receiver_phone:string,
+    receiver_adress: string,
+    parcel_qt: string,
+    parcel_content: string,
+    parcel_wight: string,
+    parcel_status: string,
+    action_by:string,
+    picked_up_time: string,
+    delivery_time: string,
+  }
+  export default User
---
+++ src/types/user.type.ts
@@ -0,0 +1,12 @@
+type User = {
+    id: number,
+    user_name: string,
+    password: string,
+    address: string,
+    user_mail: string,
+    privilege:string,
+    status: string,
+ 
+  }
+  export default User
+  
