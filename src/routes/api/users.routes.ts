18560b860fd502a515af0c36c16492caa4d49055
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-22 09:31:12 +0300

    disable authMiddleware for get all users

--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -5,10 +5,11 @@ import authMiddleware from '../../middleware/authMiddle'
 const routes = Router()
 
 routes.post('/', act.createUser)
-routes.get('/',authMiddleware, act.getAllUsers)
+routes.get('/', act.getAllUsers)
+routes.patch('/pass/:id',act.updateUserPass)
 routes.get('/:id', authMiddleware, act.getOneUser)
 routes.patch('/:id', authMiddleware,act.updateUser)
-routes.patch('/pass/:id',act.updateUserPass)
+
 routes.delete('/:id', authMiddleware, act.deleteUser)
 routes.post('/authenticate', act.authenticate)
 
