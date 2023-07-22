a651deced1a242104d7a2b338df65079c52b6ea0
Ahmed Shehata <47960845+ashehata8888@users.noreply.github.com>
2023-07-22 09:44:50 +0300

    build

--- dist/database/index.js
+++ dist/database/index.js
@@ -1,47 +1,39 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
-const express_1 = __importDefault(require("express"));
-const dotenv = __importStar(require("dotenv"));
-dotenv.config();
-const app = (0, express_1.default)();
-const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_MAX } = process.env;
-let db;
-db = new pg_1.Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: parseInt(POSTGRES_PORT),
-    max: parseInt(POSTGRES_MAX)
+const db = new pg_1.Pool({
+    connectionString: 'postgres://mkpbnnue:r8hR-PVMpADbrg0Mi1wFq0XfGBo--zcZ@surus.db.elephantsql.com/mkpbnnue',
+    // ssl: { rejectUnauthorized: false }, // Remove this line if you don't need SSL for development.
 });
-db.on('error', (error) => {
-    console.error(error.message);
+// Example query
+db.query('SELECT * FROM your_table', (err, res) => {
+    if (err) {
+        console.error('Error executing query', err);
+    }
+    else {
+        console.log('Query result:', res.rows);
+    }
 });
+// dotenv.config()
+// const app = express();
+// const {
+//   POSTGRES_HOST,
+//   POSTGRES_PORT,
+//   POSTGRES_DB,
+//   POSTGRES_USER,
+//   POSTGRES_PASSWORD,
+//   POSTGRES_MAX
+// } = process.env
+// let db: Pool
+//   db = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//     port: parseInt(POSTGRES_PORT as string),
+//     max: parseInt(POSTGRES_MAX as string)
+//   })
+// db.on('error', (error: Error) => {
+//   console.error(error.message)
+// })
 exports.default = db;
