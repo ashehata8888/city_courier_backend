4c03bbd6ba605f3e0197e685c45378a8d9f6c5de
ashehata8888 <47960845+ashehata8888@users.noreply.github.com>
2023-07-25 17:59:18 +0300

    modify db connection to local db

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,52 +4,52 @@ import * as dotenv from 'dotenv'
 
 
 
-const db = new Pool({
-  connectionString: 'postgres://mkpbnnue:r8hR-PVMpADbrg0Mi1wFq0XfGBo--zcZ@surus.db.elephantsql.com/mkpbnnue', 
-  // ssl: { rejectUnauthorized: false }, // Remove this line if you don't need SSL for development.
-});
+// const db = new Pool({
+//   connectionString: 'postgres://mkpbnnue:r8hR-PVMpADbrg0Mi1wFq0XfGBo--zcZ@surus.db.elephantsql.com/mkpbnnue', 
+//   // ssl: { rejectUnauthorized: false }, // Remove this line if you don't need SSL for development.
+// });
 
-// Example query
-db.query('SELECT * FROM users where users.id = 1', (err, res) => {
-  if (err) {
-    console.error('Error executing query', err);
-  } else {
-    console.log('Query result:', res.rows);
-  }
-});
-
-
-
-// dotenv.config()
-
-// const app = express();
-
-// const {
-//   POSTGRES_HOST,
-//   POSTGRES_PORT,
-//   POSTGRES_DB,
-//   POSTGRES_USER,
-//   POSTGRES_PASSWORD,
-//   POSTGRES_MAX
-// } = process.env
-
-// let db: Pool
-
-
-//   db = new Pool({
-//     host: POSTGRES_HOST,
-//     database: POSTGRES_DB,
-//     user: POSTGRES_USER,
-//     password: POSTGRES_PASSWORD,
-//     port: parseInt(POSTGRES_PORT as string),
-//     max: parseInt(POSTGRES_MAX as string)
-//   })
+// // Example query
+// db.query('SELECT * FROM users where users.id = 1', (err, res) => {
+//   if (err) {
+//     console.error('Error executing query', err);
+//   } else {
+//     console.log('Query result:', res.rows);
+//   }
+// });
 
 
 
-// db.on('error', (error: Error) => {
-//   console.error(error.message)
-// })
+dotenv.config()
+
+const app = express();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_MAX
+} = process.env
+
+let db: Pool
+
+
+  db = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    port: parseInt(POSTGRES_PORT as string),
+    max: parseInt(POSTGRES_MAX as string)
+  })
+
+
+
+db.on('error', (error: Error) => {
+  console.error(error.message)
+})
 
 
 
